f25326178d5a63d14bf72d8346ed10ad4489bd9e
wendy <wendyz34@nycstudents.net>
2022-09-28 20:14:29 -0400

    add print statements

---
+++ calc.java
@@ -0,0 +1,11 @@
+import.java.util.Scanner;
+public class calc {
+    public static void main(String[]args);
+    Scanner scan= new Scanner(Systen.in);
+    System.out.print("Enter value: ");
+    double totalTip= scan.nextDouble();
+    System.out.print("What is the tip in whole number without the percent sign");
+    int tipPercent = scan.nextInt();
+    System.out.print("How much people were at the party?: ");
+    int numPeople = scan.nextInt();
+}
